e173361d06bdf3b6924d28f4fd13eff496a3679c
kwibbin <krsmith2k3@gmail.com>
2025-07-27 11:31:38 -0400

    add mux

--- IAM.srcs/components/mux.vhd
+++ IAM.srcs/components/mux.vhd
@@ -21,12 +21,24 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity mux is
-    --port();
+    generic(
+        in_n : positive := 3;
+        data_width : positive := 32
+    );
+    port(
+        sel : in natural range 0 to in_n - 1;
+        in_d : in std_logic_vector(in_n * data_width - 1 downto 0);
+        out_d : out std_logic_vector(data_width - 1 downto 0)
+    );
 end mux;
 
 architecture Behavioral of mux is
 
 begin
 
+process(sel, in_d)
+begin
+    out_d <= in_d((sel + 1) * data_width - 1 downto sel * data_width);
+end process;
 
 end Behavioral;
