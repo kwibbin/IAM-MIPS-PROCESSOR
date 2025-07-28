36652bf2b2ae2f2666e069dcd792bf9d77775a7e
kwibbin <krsmith2k3@gmail.com>
2025-07-28 18:54:50 -0400

    add adder

---
+++ IAM.srcs/components/adder.vhd
@@ -0,0 +1,38 @@
+----------------------------------------------------------------------------------
+-- Engineer: kwibbin
+-- 
+-- Create Date: 07/28/2025 06:44:06 PM
+-- Design Name: 
+-- Module Name: adder - Behavioral
+-- Project Name: IAM
+-- Target Devices: Basys3 Artix 7 - XC7A35T-1CPG236C
+-- Tool Versions: Vivado 2025.1
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity adder is
+  Port (
+    in_d1, in_d2 : in std_logic_vector(31 downto 0);
+    
+out_d        : out std_logic_vector(31 downto 0)
+    );
+end adder;
+
+architecture Behavioral of adder is
+
+begin
+
+    out_d <= std_logic_vector(unsigned(in_d1) + unsigned(in_d2));
+
+end Behavioral;
