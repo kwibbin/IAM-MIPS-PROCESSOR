8d979ea82c0563c4cfecbd3cc6e96e568f2d8589
kwibbin <krsmith2k3@gmail.com>
2025-07-28 18:31:28 -0400

    add shift reg

--- IAM.srcs/components/shift_reg.vhd
+++ IAM.srcs/components/shift_reg.vhd
@@ -19,14 +19,19 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity shift_reg is
---  Port ( );
+    Port (
+        in_d  : in std_logic_vector(31 downto 0);
+        out_d : out std_logic_vector(31 downto 0)
+    );
 end shift_reg;
 
 architecture Behavioral of shift_reg is
 
 begin
 
+    out_d <= std_logic_vector(shift_left(unsigned(in_d), 2));
 
 end Behavioral;
