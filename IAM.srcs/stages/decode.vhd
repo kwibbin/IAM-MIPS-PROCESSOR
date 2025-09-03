c93070e68faf80c8a9db7eb07133b6d8d35f4af6
kwibbin <krsmith2k3@gmail.com>
2025-09-03 18:19:58 -0400

    add func decode

--- IAM.srcs/stages/decode.vhd
+++ IAM.srcs/stages/decode.vhd
@@ -1,42 +1,89 @@
 ----------------------------------------------------------------------------------
 -- Engineer: kwibbin
--- 
+--
 -- Create Date: 07/09/2025 09:15:06 PM
--- Design Name: 
+-- Design Name:
 -- Module Name: decode - Behavioral
 -- Project Name: IAM
 -- Target Devices: Basys3 Artix 7 - XC7A35T-1CPG236C
 -- Tool Versions: Vivado 2025.1
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity decode is
---  Port ( );
+    generic (
+        data_width : positive := 32;
+        addr_width : positive := 16
+    );
+    port (
+        clk        : in std_logic;
+        rst        : in std_logic;
+
+        reg_w      : in std_logic;  -- ctrl unit flag from wb
+        write_reg  : in std_logic_vector(4 downto 0); -- either instr[20:16] or instr[15:11] from wb
+        write_d    : in std_logic_vector(data_width - 1 downto 0);
+        pc_in      : in std_logic_vector(data_width - 1 downto 0);
+        pc_p4_in    : in std_logic_vector(data_width - 1 downto 0);
+        instr      : in std_logic_vector(data_width - 1 downto 0);
+
+        ctrl_flags : out std_logic_vector(11 downto 0);
+        instr_20_0 : out std_logic_vector(20 downto 0);
+        pc_out     : out std_logic_vector(data_width - 1 downto 0);
+        pc_p4_out  : out std_logic_vector(data_width - 1 downto 0);
+        reg_d_1    : out std_logic_vector(data_width - 1 downto 0);
+        reg_d_2    : out std_logic_vector(data_width - 1 downto 0);
+        branch_dst : out std_logic_vector(data_width - 1 downto 0)
+    );
 end decode;
 
 architecture Behavioral of decode is
 
+
+
 begin
 
+ctrl_unit : entity work.ctrl_unit(Behavioral)
+    port map(
+        opcode     => instr(31 downto 26),
+
+        reg_dst    => ctrl_flags(0),
+        jump       => ctrl_flags(1),
+        branch     => ctrl_flags(2),
+        mem_r      => ctrl_flags(3),
+        mem_to_reg => ctrl_flags(4),
+        alu_op     => ctrl_flags(8 downto 5),
+        mem_w      => ctrl_flags(9),
+        alu_src    => ctrl_flags(10),
+        reg_w      => ctrl_flags(11)
+    );
+
+reg_file : entity work.reg_mem(Behavioral)
+    port map(
+        reg_wr    => reg_w,
+        read_reg1 => instr(25 downto 21),
+        read_reg2 => instr(20 downto 16),
+        write_reg => write_reg,
+        write_d   => write_d,
+
+        read_d1   => reg_d_1,
+        read_d2   => reg_d_2
+    );
+
+pc_out <= pc_in;
+pc_p4_out <= pc_p4_in;
+instr_20_0 <= instr(20 downto 0);
+branch_dst <= std_logic_vector(resize(signed(instr(15 downto 0)), data_width));
 
 end Behavioral;
