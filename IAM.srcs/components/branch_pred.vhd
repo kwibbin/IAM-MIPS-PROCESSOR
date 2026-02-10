e05b5512566f5f990e97a8c768628cff652be4ee
kwibbin <krsmith2k3@gmail.com>
2026-02-09 21:16:08 -0500

    add next stage calculation

--- IAM.srcs/components/branch_pred.vhd
+++ IAM.srcs/components/branch_pred.vhd
@@ -19,18 +19,21 @@ use IEEE.NUMERIC_STD.ALL;
 use work.pc_helper.all;
 
 entity branch_pred is
+    generic (
+        addr_width  : positive := 32;
+        alignment   : std_logic_vector(3 downto 0) := "0100"
+    );
     port (
-        clk        : in std_logic;
+        clk         : in std_logic;
 
-        pc_enc_if  : in std_logic_vector(7 downto 0);
-        opcode_if  : in std_logic_vector(5 downto 0);
+        pc_if       : in std_logic_vector(addr_width - 1 downto 0);
 
-        z          : in std_logic;
-        branch_ex  : in std_logic;
-        pc_enc_ex  : in std_logic_vector(7 downto 0);
+        z           : in std_logic;
+        branch_ex   : in std_logic;
+        pc_enc_ex   : in std_logic_vector(7 downto 0);
 
-        pred_hold  : out natural range 0 to 1;
-        pred_flush : out natural range 0 to 1
+        pred_branch : out natural range 0 to 1;
+        pred_pc     : out std_logic_vector(addr_width - 1 downto 0)
     );
 end branch_pred;
 
@@ -49,15 +52,35 @@ constant store_size : positive := 256;
 type bht_store is array(0 to store_size - 1) of std_logic_vector(1 downto 0);
 signal bht : bht_store := (others => "01"); -- initialize to weak not-taken
 
+alias opcode_if : std_logic_vector(5 downto 0) is pc_if(addr_width - 1 downto 26);
+alias pc_enc_if : std_logic_vector(7 downto 0) is pc_if(9 downto 2);
+
 begin
 
 branch_if <= check_branch(opcode_if);
 pc_i_if   <= to_integer(unsigned(pc_enc_if));
 pc_i_ex   <= to_integer(unsigned(pc_enc_ex));
 
+-- determine next instr
+stage_next_instr : process(clk, branch_if, pc_i_if) begin
+    if rising_edge(clk) then
+        if branch_if = '1' then
+            if bht(pc_i_if) = "10" or bht(pc_i_if) = "11" then
+                pred_branch <= 1;
+                pred_pc <= std_logic_vector(unsigned(pc_if) + resize(unsigned(alignment), addr_width));
+            else
+                pred_branch <= 0;
+                pred_pc <= (others => '0');
+            end if;
+        else
+            pred_branch <= 0;
+            pred_pc <= (others => '0');
+        end if;
+    end if;
+end process stage_next_instr;
+
 -- retroactively update bht depending on ex branch alu result
-bht_control : process(clk, z, pc_i_ex)
-begin
+bht_control : process(clk, z, pc_i_ex) begin
     if rising_edge(clk) and branch_ex = '1' then
         -- branch taken, trend towards strong taken prediction
         if (bht(pc_i_ex) = "00" or bht(pc_i_ex) = "01" or bht(pc_i_ex) = "10") and z = '1' then
